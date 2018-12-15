8bd2133392e0b6c74fc939ac1fe21323bad6e527
clapertosa <the_wolf91@libero.it>
2018-12-15 19:42:25 +0100

    Unauthenticated users can't access to Dashboard page

--- client/pages/dashboard/index.js
+++ client/pages/dashboard/index.js
@@ -1,4 +1,6 @@
 import User from "../../hoc/User/User";
+import redirect from "../../lib/redirect";
+import checkLoggedIn from "../../lib/checkLoggedIn";
 import AvatarForm from "../../components/Dashboard/Form/AvatarForm/AvatarForm";
 import Form from "../../components/Dashboard/Form/Form";
 import styles from "./index.scss";
@@ -30,6 +32,16 @@ const Dashboard = () => {
       </User>
     </div>
   );
+};
+
+Dashboard.getInitialProps = async context => {
+  const { userLoggedIn } = await checkLoggedIn(context.apolloClient);
+
+  if (!userLoggedIn.currentUser) {
+    redirect(context, "/login");
+  }
+
+  return {};
 };
 
 export default Dashboard;
