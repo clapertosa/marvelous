bcb04447196c5dc66d186c91ae3eecc93b7f674f
clapertosa <the_wolf91@libero.it>
2019-07-21 19:03:16 +0200

    Browserslist updated

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = ctx => {
   const plugins = {
     autoprefixer: {
-      ...ctx.options.autoprefixer,
-      browsers: ["> 1%", "last 2 versions"]
+      ...ctx.options.autoprefixer
     }
   };
   plugins["postcss-nested"] = {};
